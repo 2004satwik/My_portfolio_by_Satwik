2dd386a2659f3992b345d9547364dae43544f30a
Satwik_Bhat <satwikbhat121@gmail.com>
2025-07-27 19:35:23 +0530

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/test-portfolio/',
+    base: '/My_portfolio_by_Satwik/',
     plugins: [react()],
     build: {
         rollupOptions: {
